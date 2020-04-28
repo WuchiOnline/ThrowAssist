16187d130bc8ef9f6571ae2b648d80beff9081d4
Eric Wu <eric@wuchi.online>
2020-04-27 18:24:20 -0700

    XRGrabInteractable configured for inheritance

---
+++ Assets/WuchiOnline/Scripts/Wuchi_ThrowAssist.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Wuchi_ThrowAssist : XRGrabInteractable
+{
+
+    List<Vector3> pollingList;
+
+    void Start()
+    {
+        pollingList = new List<Vector3>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    protected override void Detach()
+    {
+        if (m_ThrowOnDetach)
+        {
+            m_RigidBody.velocity = m_DetachVelocity;
+            m_RigidBody.angularVelocity = m_DetachAngularVelocity;
+        }
+
+        Debug.Log("Test");
+    }
+}
