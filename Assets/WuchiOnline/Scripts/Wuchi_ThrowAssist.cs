333a698cceb28bb9e435d863d2ddef72d4f59c0c
Eric Wu <eric@wuchi.online>
2020-04-27 19:31:41 -0700

    OnSelectEnter and OnSelectExit overrides converted

--- Assets/WuchiOnline/Scripts/Wuchi_ThrowAssist.cs
+++ Assets/WuchiOnline/Scripts/Wuchi_ThrowAssist.cs
@@ -5,6 +5,36 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class Wuchi_ThrowAssist : XRGrabInteractable
 {
+
+    bool isControllerVelocityPollingActive;
+
+    List<Vector3> polledVelocities;
+
+    public void Start()
+    {
+        polledVelocities = new List<Vector3>();
+    }
+
+    protected override void OnSelectEnter(XRBaseInteractor interactor)
+    {
+        if (!interactor)
+            return;
+        base.OnSelectEnter(interactor);
+
+        isControllerVelocityPollingActive = true;
+        Debug.Log("Grabbed.");
+
+    }
+
+    protected override void OnSelectExit(XRBaseInteractor interactor)
+    {
+        base.OnSelectExit(interactor);
+
+        isControllerVelocityPollingActive = false;
+        Debug.Log("Ungrabbed.");
+
+    }
+
     protected override void Detach()
     {
         base.Detach();
