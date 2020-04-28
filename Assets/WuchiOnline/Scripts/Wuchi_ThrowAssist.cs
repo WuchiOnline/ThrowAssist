6a92aa89929533ff900bea0b1b378dd8fd96900f
Eric Wu <eric@wuchi.online>
2020-04-27 19:47:26 -0700

    Update Wuchi_ThrowAssist.cs

--- Assets/WuchiOnline/Scripts/Wuchi_ThrowAssist.cs
+++ Assets/WuchiOnline/Scripts/Wuchi_ThrowAssist.cs
@@ -35,9 +35,26 @@ public class Wuchi_ThrowAssist : XRGrabInteractable
 
     }
 
-    protected override void Detach()
+    //// VRTK's ProcessUpdate method runs in every Update on the Interactable Object while it is being grabbed.
+    //public override void ProcessUpdate()
+    //{
+    //    if (isControllerVelocityPollingActive)
+    //    {
+    //        VRTK_ControllerReference controllerReference = VRTK_ControllerReference.GetControllerReference(grabbedObjectScript.GetGrabbingObject());
+    //        Vector3 velocity = VRTK_DeviceFinder.GetControllerVelocity(controllerReference);
+    //        pollingList.Add(velocity);
+    //    }
+    //}
+
+
+    protected override void Detach() // Throw velocity upon detach (ungrab).
     {
-        base.Detach();
-        Debug.Log("Tester.");
+        if (m_ThrowOnDetach)
+        {
+            m_RigidBody.velocity = m_DetachVelocity;
+            m_RigidBody.angularVelocity = m_DetachAngularVelocity;
+        }
+
+        Debug.Log("Detached.");
     }
 }
