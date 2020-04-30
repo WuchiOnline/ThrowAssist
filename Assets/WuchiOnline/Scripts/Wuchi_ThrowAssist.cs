05e4cfee9c941b6cb67968a2ca4de53ffec0ad91
Eric Wu <eric@wuchi.online>
2020-04-30 14:50:37 -0700

    Added Rig To Target Transform object

--- Assets/WuchiOnline/Scripts/Wuchi_ThrowAssist.cs
+++ Assets/WuchiOnline/Scripts/Wuchi_ThrowAssist.cs
@@ -6,12 +6,28 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class Wuchi_ThrowAssist : XRGrabInteractable
 {
-    public int pollsToProcess = 3;
-    public float ThrowStrengthAssistThreshold = 0.45f;
+
+    // Magic Numbers: all constants were determined by extensive playtesting for best feel.
+    private const float HorizontalAssistThreshold = 0.75f;
+    private const float MinUpwardThrowModifier = 15.0f;
+    private const float MaxUpwardThrowModifier = 1.54f;
+    private const float MinForwardThrowModifier = 13.0f;
+    private const float MaxForwardThrowModifier = 1.36f;
+    private const float MinBaseAssistRange = 0.5f;
+    private const float MaxBaseAssistRange = 5.5f;
+    private const int OptimalPolledVelocityCount = 3; // Three is the sweet spot, although four and five produce decent results as well.
+    private const float AverageReleaseHeight = 2.5f;
+    private const float ThrowStrengthAssistThreshold = 0.45f;
+
+    public Transform target;
+
+    public float unassistedThrowVelocityModifier;
 
     bool isInteractorVelocityPollingActive;
 
     public Queue<Vector3> polledVelocities;
+
+    private Vector3 baseThrowVelocity;
 
     public void Start()
     {
@@ -45,7 +61,7 @@ public class Wuchi_ThrowAssist : XRGrabInteractable
             Vector3 smoothedVelocity = getSmoothedVelocityValue(m_ThrowSmoothingVelocityFrames);
             Vector3 velocityToPoll = smoothedVelocity * m_ThrowVelocityScale;
 
-            if(polledVelocities.Count >= pollsToProcess)
+            if(polledVelocities.Count >= OptimalPolledVelocityCount)
             {
                 polledVelocities.Dequeue();
             }
@@ -57,7 +73,6 @@ public class Wuchi_ThrowAssist : XRGrabInteractable
 
     }
 
-
     protected override void Detach() // Throw velocity upon detach (ungrab).
     {
         if (m_ThrowOnDetach)
@@ -65,6 +80,7 @@ public class Wuchi_ThrowAssist : XRGrabInteractable
             // RESUME HERE:
             // 4-28-20 You need to figure out how to abstract this logic so that it works from any position/rotation in the scene,
             // not just predetermined transforms with preset rotations towards the target object.
+
             // DetermineCurrentPlayerPositionAnchor();
 
             if (m_DetachVelocity.y < ThrowStrengthAssistThreshold || m_DetachVelocity.z < ThrowStrengthAssistThreshold || polledVelocities.Count < 1) // Does not meet the minimum throw strength to trigger assisted throw.
@@ -85,6 +101,87 @@ public class Wuchi_ThrowAssist : XRGrabInteractable
         Debug.Log("Detached.");
     }
 
+    private void DetermineCurrentPlayerPositionAnchor()
+    {
+        // This logic needs to be abstracted.
+        // This may be that you create a new currentPlayerTransform that has everything at 0 except for the rotation of the headset.
+        // logic abstraction attempt 1:
+
+        //Vector3 direction = (target.position - transform.position).normalized;
+
+        //// create the rotation we need to be in to look at the target
+        //Quaternion lookAtRotation = Quaternion.LookRotation(direction);
+
+        //Quaternion lookAtRotation_onlyY = Quaternion.Euler(transform.rotation.eulerAngles.x, lookAtRotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+        //transform.rotation = lookAtRotation_onlyY;
+
+
+        //switch (playerPositionTrackerScript.currentPlayerPosition)
+        //{
+        //    case 1:
+        //        currentPlayerTransform = playerPositionA;
+        //        break;
+        //    case 2:
+        //        currentPlayerTransform = playerPositionB;
+        //        break;
+        //    case 3:
+        //        currentPlayerTransform = playerPositionC;
+        //        break;
+        //    case 4:
+        //        currentPlayerTransform = playerPositionD;
+        //        break;
+        //    case 5:
+        //        currentPlayerTransform = playerPositionE;
+        //        break;
+        //}
+    }
+
+    //public void ConstructPlayerToTargetTransformAnchor()
+    //{
+    //    Vector3 direction = (target.position - transform.position).normalized;
+
+    //    //create the rotation we need to be in to look at the target
+    //    Quaternion lookAtRotation = Quaternion.LookRotation(direction);
+
+    //    Quaternion lookAtRotation_onlyY = Quaternion.Euler(transform.rotation.eulerAngles.x, lookAtRotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+
+    //    transform.rotation = lookAtRotation_onlyY;
+    //}
+
+    //private Vector3 DetermineAssistedThrowVelocity()
+    //{
+    //    baseThrowVelocity = DetermineHighestUpwardVelocityFromPolledList();
+
+    //    float assistedUpwardVelocity = AssistBaseUpwardVelocity();
+    //    float assistedForwardVelocity = AssistBaseForwardVelocity();
+    //    float adjustedHorizontalVelocity = AdjustBaseHorizontalVelocity(assistedForwardVelocity);
+
+    //    Vector3 newAssistedThrowVelocity = new Vector3(adjustedHorizontalVelocity, assistedUpwardVelocity, assistedForwardVelocity);
+
+    //    // resume here. 4-30-20 12:48 PM
+    //    // transform world space velocity so that it is localized to a transform that is rotated (looking at) target object.
+
+    //    Vector3 transformedAssistedThrowVelocity = currentPlayerTransform.InverseTransformVector(newAssistedThrowVelocity);
+
+    //    float releaseHeightThrowModifier;
+
+    //    if (controllerPositionAtRelease.y > AverageReleaseHeight) // Above-Average Release Height 
+    //    {
+    //        releaseHeightThrowModifier = 1.0f + ((AverageReleaseHeight - controllerPositionAtRelease.y) * 0.05f); // This function was determined through extensive playtesting for best feel 
+    //    }
+    //    else // Below-Average Release Height
+    //    {
+    //        releaseHeightThrowModifier = 1.0f + ((AverageReleaseHeight - controllerPositionAtRelease.y) * 0.06f); // This function was determined through extensive playtesting for best feel
+    //    }
+
+    //    Vector3 finalAssistedThrowVelocity = transformedAssistedThrowVelocity * releaseHeightThrowModifier;
+
+    //    pollingList.Clear();
+
+    //    return finalAssistedThrowVelocity;
+    //}
+
     private Vector3 DetermineHighestUpwardVelocityFromPolledList()
     {
 
@@ -93,6 +190,72 @@ public class Wuchi_ThrowAssist : XRGrabInteractable
         Debug.Log("Highest Upward Velocity From Polled List is: " + highestUpwardVelocity);
 
         return highestUpwardVelocity;
+
+    }
+
+    private float AssistBaseUpwardVelocity()
+    {
+        if (baseThrowVelocity.y <= 0f)
+        {
+            return baseThrowVelocity.y * unassistedThrowVelocityModifier;
+        }
+        else if (baseThrowVelocity.y < MinBaseAssistRange)
+        {
+            return baseThrowVelocity.y * MinUpwardThrowModifier; // This constant was determined through extensive playtesting for best feel
+        }
+        else if (baseThrowVelocity.y < MaxBaseAssistRange)
+        {
+            return AssistedBaseUpwardMagnitude();
+        }
+        else
+        {
+            return baseThrowVelocity.y * MaxUpwardThrowModifier; // This constant was determined through extensive playtesting for best feel
+        }
+    }
+
+    private float AssistedBaseUpwardMagnitude()
+    {
+        // This function was determined through extensive playtesting for best feel, please see: https://www.desmos.com/calculator/clrzceq5ch
+        return (baseThrowVelocity.y / 6.0f) + 7.5f;
+    }
+
+    private float AssistBaseForwardVelocity()
+    {
+        if (baseThrowVelocity.z <= 0f)
+        {
+            return baseThrowVelocity.z * unassistedThrowVelocityModifier;
+        }
+        else if (baseThrowVelocity.z < MinBaseAssistRange) // This constant was determined through extensive playtesting for best feel
+        {
+            return baseThrowVelocity.z * MinForwardThrowModifier;
+        }
+        else if (baseThrowVelocity.z < MaxBaseAssistRange)
+        {
+            return AssistedBaseForwardMagnitude();
+        }
+        else
+        {
+            return baseThrowVelocity.z * MaxForwardThrowModifier; // This constant was determined through extensive playtesting for best feel
+        }
+    }
+
+    private float AssistedBaseForwardMagnitude()
+    {
+        // This function was determined through extensive playtesting for best feel, please see: https://www.desmos.com/calculator/m07laliezy)
+        return (baseThrowVelocity.z / 6.0f) + 6.5f;
+    }
+
+    private float AdjustBaseHorizontalVelocity(float assistedForwardVelocity)
+    {
+        // If throw is not within horizontal threshold of target object, then adjust accordingly to make throw more accurate.
+        if (baseThrowVelocity.x > HorizontalAssistThreshold || baseThrowVelocity.x < -1 * HorizontalAssistThreshold)
+        {
+            return (baseThrowVelocity.x * assistedForwardVelocity) / baseThrowVelocity.z;
+        }
+        else
+        {
+            return baseThrowVelocity.x;
+        }
 
     }
 }
