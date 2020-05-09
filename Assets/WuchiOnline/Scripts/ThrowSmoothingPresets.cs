6db7f6597c19032a2588b1f0fab555033266393e
Eric Wu <eric@wuchi.online>
2020-05-09 14:15:12 -0700

    ScriptableObject ThrowSmoothingPresets successfully implemented

---
+++ Assets/WuchiOnline/Scripts/ThrowSmoothingPresets.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Presets", menuName = "Throw Smoothing Presets")]
+public class ThrowSmoothingPresets : ScriptableObject
+{
+    // The optimal amount of velocities to poll and utilize when assisting velocity transformations.
+    public int optimalPolledVelocityCount;
+
+    // The minimum amount of magnitude a throw must have in either forward and upward directions to be eligible for the smoothing algorithm.
+    public float throwStrengthThreshold;
+    // The maximum amount of horizontal inaccuracy a throw must not exceed to be eligible for the smoothing algorithm.
+    public float normalizedHorizontalInaccuracyThreshold;
+    // The minimum local velocity threshold a throw's magnitude must exceed to be assisted.
+    public float minLocalAssistThreshold;
+    // The maximum local velocity threshold a throw's magnitude must not exceed to be assisted.
+    public float maxLocalAssistThreshold;
+    // The minimum amount of horizontal inaccuracy (relative to target object) a throw must have to warrant proportionately adjusting with assisted forward velocity.
+    public float horizontalAdjustThreshold;
+
+    // The upward velocity modifier applied when a throw is below the minimum local velocity assist threshold.
+    public float minUpwardThrowModifier;
+    // The upward velocity modifier applied when a throw is above the maximum local velocity assist threshold.
+    public float maxUpwardThrowModifier;
+    // The forward velocity modifier applied when a throw is below the minimum local velocity assist threshold.
+    public float minForwardThrowModifier;
+    // The forward velocity modifier applied when a throw is above the maximum local velocity assist threshold.
+    public float maxForwardThrowModifier;
+
+    // The average release height of a throw, used to modify the smoothed velocity for users of all heights and wingspans.
+    public float averageReleaseHeight;
+    public float aboveAverageReleaseHeightModifier;
+    public float belowAverageReleaseHeightModifier;
+
+    // Optional strength modifier for unassisted throws.
+    public float unassistedThrowVelocityModifier;
+}
--- Assets/WuchiOnline/Scripts/Wuchi_ThrowAssist.cs
+++ Assets/WuchiOnline/Scripts/Wuchi_ThrowAssist.cs
@@ -9,42 +9,45 @@ using UnityEngine.XR.Interaction.Toolkit;
 /// Originally built and implemented as the core interaction for Hooplord, a PC VR game centered around basketball shooting mechanics.
 /// </summary>
 
-public class Wuchi_ThrowAssist : XRGrabInteractable
+public class Wuchi_ThrowAssist : XRGrabInteractable // XR Interaction Toolkit uses Hungarian notation, which has been matched here for consistency.
 {
-    // The Magic Numbers: All constants were determined by extensive playtesting for best feel.
-    // These can be abstracted and fine-tuned to achieve a wide variety of smoothing results.
+    // The default values provided were determined by extensive playtesting for best feel.
+    // These can be experimented with and fine-tuned to achieve a wide variety of smoothing results.
+
+    // If you discover a new smoothing effect and want to save the values as presets to initialize on Start():
+    // Create a new Throw Smoothing Presets ScriptableObject instance by right-clicking in your Project window > Create > Throw Smoothing Presets.
+    public ThrowSmoothingPresets throwSmoothingPreset;
 
     // The optimal amount of velocities to poll and utilize when assisting velocity transformations.
-    const int k_OptimalPolledVelocityCount = 4;
+    public int m_OptimalPolledVelocityCount = 4;
 
     // The minimum amount of magnitude a throw must have in either forward and upward directions to be eligible for the smoothing algorithm.
-    const float k_ThrowStrengthThreshold = 0.45f;
+    public float m_ThrowStrengthThreshold = 0.45f;
     // The maximum amount of horizontal inaccuracy a throw must not exceed to be eligible for the smoothing algorithm.
-    const float k_NormalizedHorizontalInaccuracyThreshold = 0.2f;
+    public float m_NormalizedHorizontalInaccuracyThreshold = 0.2f;
     // The minimum local velocity threshold a throw's magnitude must exceed to be assisted.
-    const float k_MinLocalAssistThreshold = 0.5f;
+    public float m_MinLocalAssistThreshold = 0.5f;
     // The maximum local velocity threshold a throw's magnitude must not exceed to be assisted.
-    const float k_MaxLocalAssistThreshold = 5.5f;
+    public float m_MaxLocalAssistThreshold = 5.5f;
     // The minimum amount of horizontal inaccuracy (relative to target object) a throw must have to warrant proportionately adjusting with assisted forward velocity.
-    const float k_HorizontalAdjustThreshold = 0.75f;
+    public float m_HorizontalAdjustThreshold = 0.75f;
 
     // The upward velocity modifier applied when a throw is below the minimum local velocity assist threshold.
-    const float k_MinUpwardThrowModifier = 15.0f;
+    public float m_MinUpwardThrowModifier = 15.0f;
     // The upward velocity modifier applied when a throw is above the maximum local velocity assist threshold.
-    const float k_MaxUpwardThrowModifier = 1.54f;
+    public float m_MaxUpwardThrowModifier = 1.54f;
     // The forward velocity modifier applied when a throw is below the minimum local velocity assist threshold.
-    const float k_MinForwardThrowModifier = 13.0f;
+    public float m_MinForwardThrowModifier = 13.0f;
     // The forward velocity modifier applied when a throw is above the maximum local velocity assist threshold.
-    const float k_MaxForwardThrowModifier = 1.36f;
+    public float m_MaxForwardThrowModifier = 1.36f;
 
     // The average release height of a throw, used to modify the smoothed velocity for users of all heights and wingspans.
-    const float k_AverageReleaseHeight = 2.5f;
-    const float k_AboveAverageReleaseHeightModifier = 0.05f;
-    const float k_BelowAverageReleaseHeightModifier = 0.06f;
+    public float m_AverageReleaseHeight = 2.5f;
+    public float m_AboveAverageReleaseHeightModifier = 0.05f;
+    public float m_BelowAverageReleaseHeightModifier = 0.06f;
 
     // Optional strength modifier for unassisted throws.
-    [SerializeField]
-    float m_UnassistedThrowVelocityModifier = 1.0f;
+    public float m_UnassistedThrowVelocityModifier = 1.0f;
 
     // Retains Interactor's Attach Transform reference for Detach() in LateUpdate.
     Transform m_CurrentInteractorAttach;
@@ -61,8 +64,30 @@ public class Wuchi_ThrowAssist : XRGrabInteractable
 
     void Start()
     {
+        InitializeThrowSmoothingPresets();
         m_PolledVelocities = new Queue<Vector3>();
         rigToTarget = GameObject.FindWithTag("RigToTarget").transform;
+    }
+
+    void InitializeThrowSmoothingPresets()
+    {
+        if (throwSmoothingPreset != null)
+        {
+            m_OptimalPolledVelocityCount = throwSmoothingPreset.optimalPolledVelocityCount;
+            m_ThrowStrengthThreshold = throwSmoothingPreset.throwStrengthThreshold;
+            m_NormalizedHorizontalInaccuracyThreshold = throwSmoothingPreset.normalizedHorizontalInaccuracyThreshold;
+            m_MinLocalAssistThreshold = throwSmoothingPreset.minLocalAssistThreshold;
+            m_MaxLocalAssistThreshold = throwSmoothingPreset.maxLocalAssistThreshold;
+            m_HorizontalAdjustThreshold = throwSmoothingPreset.horizontalAdjustThreshold;
+            m_MinUpwardThrowModifier = throwSmoothingPreset.minUpwardThrowModifier;
+            m_MaxUpwardThrowModifier = throwSmoothingPreset.maxUpwardThrowModifier;
+            m_MinForwardThrowModifier = throwSmoothingPreset.minForwardThrowModifier;
+            m_MaxForwardThrowModifier = throwSmoothingPreset.maxForwardThrowModifier;
+            m_AverageReleaseHeight = throwSmoothingPreset.averageReleaseHeight;
+            m_AboveAverageReleaseHeightModifier = throwSmoothingPreset.aboveAverageReleaseHeightModifier;
+            m_BelowAverageReleaseHeightModifier = throwSmoothingPreset.belowAverageReleaseHeightModifier;
+            m_UnassistedThrowVelocityModifier = throwSmoothingPreset.unassistedThrowVelocityModifier;
+        }
     }
 
     protected override void OnSelectEnter(XRBaseInteractor interactor)
@@ -87,7 +112,7 @@ public class Wuchi_ThrowAssist : XRGrabInteractable
             Vector3 smoothedVelocity = getSmoothedVelocityValue(m_ThrowSmoothingVelocityFrames);
             Vector3 velocityToPoll = smoothedVelocity * m_ThrowVelocityScale;
 
-            if(m_PolledVelocities.Count >= k_OptimalPolledVelocityCount)
+            if(m_PolledVelocities.Count >= m_OptimalPolledVelocityCount)
             {
                 m_PolledVelocities.Dequeue();
             }
@@ -103,9 +128,9 @@ public class Wuchi_ThrowAssist : XRGrabInteractable
             UpdateRigToTargetRotation();
 
             // Evaluate if throw is both strong and accurate enough to warrant applying the smoothing algorithm.
-            if (m_DetachVelocity.y < k_ThrowStrengthThreshold || // Throw does not meet the minimum vertical throw strength to warrant smoothing.
-                rigToTarget.InverseTransformVector(m_DetachVelocity).z < k_ThrowStrengthThreshold || // Throw does not meet the minimum forward throw strength to warrant smoothing. 
-                Mathf.Abs(rigToTarget.InverseTransformVector(m_DetachVelocity).normalized.x) > k_NormalizedHorizontalInaccuracyThreshold || // Throw is not horizontally accurate enough to warrant smoothing.
+            if (m_DetachVelocity.y < m_ThrowStrengthThreshold || // Throw does not meet the minimum vertical throw strength to warrant smoothing.
+                rigToTarget.InverseTransformVector(m_DetachVelocity).z < m_ThrowStrengthThreshold || // Throw does not meet the minimum forward throw strength to warrant smoothing. 
+                Mathf.Abs(rigToTarget.InverseTransformVector(m_DetachVelocity).normalized.x) > m_NormalizedHorizontalInaccuracyThreshold || // Throw is not horizontally accurate enough to warrant smoothing.
                 m_PolledVelocities.Count < 1) // Object's velocity was not polled for at least a single frame before release.
             {
                 m_RigidBody.velocity = m_DetachVelocity;
@@ -162,17 +187,17 @@ public class Wuchi_ThrowAssist : XRGrabInteractable
         {
             return localVelocity.y * m_UnassistedThrowVelocityModifier;
         }
-        else if (localVelocity.y < k_MinLocalAssistThreshold)
+        else if (localVelocity.y < m_MinLocalAssistThreshold)
         {
-            return localVelocity.y * k_MinUpwardThrowModifier;
+            return localVelocity.y * m_MinUpwardThrowModifier;
         }
-        else if (localVelocity.y < k_MaxLocalAssistThreshold)
+        else if (localVelocity.y < m_MaxLocalAssistThreshold)
         {
             return AssistLocalUpwardMagnitude(localVelocity);
         }
         else
         {
-            return localVelocity.y * k_MaxUpwardThrowModifier;
+            return localVelocity.y * m_MaxUpwardThrowModifier;
         }
     }
 
@@ -188,17 +213,17 @@ public class Wuchi_ThrowAssist : XRGrabInteractable
         {
             return localVelocity.z * m_UnassistedThrowVelocityModifier;
         }
-        else if (localVelocity.z < k_MinLocalAssistThreshold)
+        else if (localVelocity.z < m_MinLocalAssistThreshold)
         {
-            return localVelocity.z * k_MinForwardThrowModifier;
+            return localVelocity.z * m_MinForwardThrowModifier;
         }
-        else if (localVelocity.z < k_MaxLocalAssistThreshold)
+        else if (localVelocity.z < m_MaxLocalAssistThreshold)
         {
             return AssistLocalForwardMagnitude(localVelocity);
         }
         else
         {
-            return localVelocity.z * k_MaxForwardThrowModifier;
+            return localVelocity.z * m_MaxForwardThrowModifier;
         }
     }
 
@@ -210,7 +235,7 @@ public class Wuchi_ThrowAssist : XRGrabInteractable
 
     float AdjustLocalHorizontalVelocity(Vector3 localVelocity, float assistedForwardVelocity)
     {
-        if (localVelocity.x > k_HorizontalAdjustThreshold || localVelocity.x < -1 * k_HorizontalAdjustThreshold)
+        if (localVelocity.x > m_HorizontalAdjustThreshold || localVelocity.x < -1 * m_HorizontalAdjustThreshold)
         {
             return (localVelocity.x * assistedForwardVelocity) / localVelocity.z;
         }
@@ -224,13 +249,13 @@ public class Wuchi_ThrowAssist : XRGrabInteractable
     Vector3 ApplyReleaseHeightModifier(Vector3 assistedVelocity)
     {
         float releaseHeightThrowModifier;
-        if (m_CurrentInteractorAttach.position.y > k_AverageReleaseHeight) // Above-Average Release Height 
+        if (m_CurrentInteractorAttach.position.y > m_AverageReleaseHeight) // Above-Average Release Height 
         {
-            releaseHeightThrowModifier = 1.0f + ((k_AverageReleaseHeight - m_CurrentInteractorAttach.position.y) * k_AboveAverageReleaseHeightModifier);
+            releaseHeightThrowModifier = 1.0f + ((m_AverageReleaseHeight - m_CurrentInteractorAttach.position.y) * m_AboveAverageReleaseHeightModifier);
         }
         else // Below-Average Release Height
         {
-            releaseHeightThrowModifier = 1.0f + ((k_AverageReleaseHeight - m_CurrentInteractorAttach.position.y) * k_BelowAverageReleaseHeightModifier);
+            releaseHeightThrowModifier = 1.0f + ((m_AverageReleaseHeight - m_CurrentInteractorAttach.position.y) * m_BelowAverageReleaseHeightModifier);
         }
 
         return assistedVelocity * releaseHeightThrowModifier;
